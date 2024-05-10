0d84a795c6960c55ece199b6b0e2158b07c807f4
Mia.Meyer <miameyer15@gmail.com>
2024-05-10 12:16:49 +0200

    Get started on the Python script

---
+++ PythonScript.py
@@ -0,0 +1,54 @@
+import numpy as np #deal with arrays in python
+import pandas as pd #python data frames
+import statsmodels.api as sm  # GLM
+import statsmodels.formula.api as smf  # formula based GLM
+import matplotlib.pyplot as plt #standard pythonic approach for plots
+import seaborn as sns #nice library for plots
+
+msdata = pd.read_csv(
+    "glms_meyershi.csv",
+    dtype = {
+        'acc_year': int,
+        'dev_year': int,
+        'incremental': float,
+        'cumulative': float},
+        delimiter=';'
+    )
+print(msdata)
+
+# Drop the first column ('team')
+msdata.drop(columns=msdata.columns[0], axis=1, inplace=True)
+
+fig, axs = plt.subplots(1, 2)
+plt.subplots_adjust(wspace=1)
+g=sns.lineplot(x='dev_year',y='cumulative',data=msdata,hue='acc_year',palette=sns.color_palette("deep"), ax=axs[0])
+g.legend(loc='center left', bbox_to_anchor=(1, 0.5),fontsize = 6)
+g.set_title("Cumulative payments")
+
+g1=sns.lineplot(x='dev_year',y='incremental',data=msdata,hue='acc_year',palette=sns.color_palette("deep"), ax=axs[1])
+g1.legend(loc='center left', bbox_to_anchor=(1, 0.5),fontsize = 6)
+g1.set_title("Incremental payments")
+
+plt.show()
+
+#  add some additional variables to the data set
+
+cat_type = pd.api.types.CategoricalDtype(categories=list(range(1,11)), ordered=True)
+
+msdata["acc_year_factor"] = msdata["acc_year"].astype(cat_type)
+msdata["dev_year_factor"] = msdata["dev_year"].astype(cat_type)
+msdata["cal_year"] = msdata["acc_year"] + msdata["dev_year"] - 1  # subtract 1 so that first cal_year is 1 not 2
+
+print(msdata.head(6))
+print(msdata.dtypes)
+
+
+######################################################################
+####################### CHAIN LADDER MODEL ###########################
+######################################################################
+
+                    ## FITTING THE MODEL ##
+glm_fit1 = smf.glm('incremental~ -1 + acc_year_factor + dev_year_factor',
+                   data = msdata,
+                   family = sm.families.Poisson() ).fit(scale='X2')
+print(glm_fit1.summary())
